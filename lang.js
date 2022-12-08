913b917c0674d2d8dcef0f4a6bddc8261c8567ae
霊音・永旻 <109428665+eimirein@users.noreply.github.com>
2022-12-08 23:08:14 +0400

    Add files via upload

--- lang.js
+++ lang.js
@@ -36,14 +36,16 @@ assets.russian = {
 	22: `ПОЛУЧИТЕ ПОДРОБНОЕ КОММЕРЧЕСКОЕ ПРЕДЛОЖЕНИЕ`,
 	23: `Рассчитать стоимость`,
 	24: `Свяжитесь с нами`,
-	25: `<x style='font-weight: 600; color: var(--accent)'>Номер для связи:</x><br>
-	<x class='ref' onclick='href("tel:+79277888882")'>+7 (927) 788-88-82</x>`,
+	25: `<x style='font-weight: 600; color: var(--accent)'>Связь с руководством:</x><br>
+	<x class='ref' onclick='href("tel:+79277888882")'>+7 (927) 788-88-82</x><br>
+	<x class='ref' onclick='href("https://t.me/+79277888882")'>t.me/asvorobyov</x><br>
+	<x class='ref' onclick='href("https://wa.me/79277888882")'>wa.me/79277888882</x>`,
 	26: `<x style='font-weight: 600; color: var(--accent)'>E-mail:</x><br>
 	<x class='ref' onclick='href("mailto: AVorobyovS@ya.ru")'>AVorobyovS@ya.ru</x>`,
 	27: `<x style='font-weight: 600; color: var(--accent)'>Мы в соцсетях:</x><br>
-	<x class='ref' onclick='href("https://vk.com/alvirity")'>https://vk.com/alvirity</x><br>
-	<x class='ref' onclick='href("https://vk.com/alvirity.studio")'>https://vk.com/alvirity.studio</x><br>
-	<x class='ref' onclick='href("https://t.me/generatorofthefuture")'>https://t.me/generatorofthefuture</x>`,
+	<x class='ref' onclick='href("https://vk.com/alvirity")'>vk.com/alvirity</x><br>
+	<x class='ref' onclick='href("https://vk.com/alvirity.studio")'>vk.com/alvirity.studio</x><br>
+	<x class='ref' onclick='href("https://t.me/generatorofthefuture")'>t.me/generatorofthefuture</x>`,
 	28: `<x style='font-weight: 600; color: var(--accent)'>Главный офис:</x><br>
 	<x class='ref' onclick='href("https://goo.gl/maps/qCFrD4rxLQt4912z9")'>г. Тольятти, ул. Южное шоссе, д. 163А к2/4</x>`,
 	29: `Я согласен(а) на обработку персональных данных`,
@@ -51,56 +53,49 @@ assets.russian = {
 	31: ``,
 	},
 	p2t: {
-		0: `<x style='font: var(--fontTitle)'>Что мы можем предложить</x><br><br>
-		«АльВиРити» имеет опыт в сферах:<br>VR разработки, ПО, веб-сайтов, мобильных приложений, графического дизайна, маркетинге.`,
-		1: `VR/AR разработка`,
-		2: `Наши специалисты создадут для вас лучший проект в области виртуальной и дополненной реальности`,
-		3: `Мобильные приложения`,
-		4: `Это быстрое и удобное ПО, предназначенное для работы на смартфонах, планшетах и других мобильных устройствах`,
-		5: `Разработка ПО`,
-		6: `Для написания программного обеспечения мы основываемся на методологии разработки.
-		<br>Индивидуальное Програмное обеспечение увеличит прибыль до 50%`,
-		7: `Веб-разработка`,
-		8: `Качественная Frontend и Backend разработка - залог успешной работы вашего веб-проекта.
-		<br>Сделайте заказ комплексных услуг по созданию сайта - переведите ваш бизнес на новый уровень`,
-		9: `Портреты Pop-Art`,
-		10: `Создадим ваш индивидуальный портрет в Pop-Art обработке`,
-		11: `Еще больше ожидается...`,
-		12: ``,
-		13: ``,
-		14: ``,
+	0: `<x style='font: var(--fontTitle)'>Что мы можем предложить</x><br><br>
+	«АльВиРити» имеет опыт в сферах:<br>VR разработки, ПО, веб-сайтов, мобильных приложений, графического дизайна, маркетинге.`,
+	1: `VR/AR разработка`,
+	2: `Наши специалисты создадут для вас лучший проект в области виртуальной и дополненной реальности`,
+	3: `Мобильные приложения`,
+	4: `Это быстрое и удобное ПО, предназначенное для работы на смартфонах, планшетах и других мобильных устройствах`,
+	5: `Разработка ПО`,
+	6: `Для написания программного обеспечения мы основываемся на методологии разработки.
+	<br>Индивидуальное Програмное обеспечение увеличит прибыль до 50%`,
+	7: `Веб-разработка`,
+	8: `Качественная Frontend и Backend разработка - залог успешной работы вашего веб-проекта.
+	<br>Сделайте заказ комплексных услуг по созданию сайта - переведите ваш бизнес на новый уровень`,
+	9: `Портреты Pop-Art`,
+	10: `Создадим ваш индивидуальный портрет в Pop-Art обработке`,
+	11: `Еще больше ожидается...`,
+	12: ``,
+	13: ``,
+	14: ``,
 	},
 	p3t: {
-		0: `Наше Портфолио`,
-		1: `Сайт АльВиРити Studio`,
-		2: `Представляем вашему вниманию один из сайтов IT компании «АльВиРити».<br><br>
-		Представленный сайт позволит вам ознакомится поближе со спецификой работы компании, 
-		и так же вы получите возможность познакомится со всеми членами дружной команды «АльВиРити».`,
-		3: `Сайт АльВиРити VR/AVR`,
-		4: `Представляем вашему вниманию второй сайт нашей IT компании.<br><br>
-		На сайте вы можете заказать комплект оказываемых услуг.`,
-		5: `Сайт руководителя компании «Алексея Воробьева»`,
-		6: `Данный сайт создавался лично для руководителя IT компании «АльВиРити», Алексея Воробьева.<br><br>
-		На сайте вы можете познакомиться с его автобиографией.`,
-		7: `Сайт компании «ТИС»`,
-		8: `Сайт для компании ООО «ТИС».<br><br>
-		Данная компания занимается производством качественных уплотнений и пластин для пластичных теплообменников.`,
-		9: `Avia-realtor, Montenegro`,
-		10: `Cайт разрабатывался компанией, занимающейся оформлением вида на жительство и покупкой недвижимости в Черногории и Турции.`,
-		11: `Сайт фитнес-центра «NEO»`,
-		12: `Сайт для фитнес центра «NEO» разработан по заказу технопарка в сфере высоких технологий "Жигулёвская долина".<br><br>
-		Мы также внедрили ПО для автоматизации администрационных процессов компании.`,
-		13: `Сайт по оказанию адвокатских услуг`,
-		14: `Сайт создавался опытными дизайнерам для многоуважаемого адвоката Русяевой Ольги Александровны.`,
-		15: `Сайт «Центр кадастровой оценки<br>Липецкой области»`,
-		16: `Сайт разрабатывался для «Центра кадастровой оценки Липецкой области».`,
-	},
-	fooP: {
-		1: 'Резидент Технопарка Жигулёвская долина',
-		2: 'КОНТАКТЫ',
-		3: 'Телефон:',
-		4: 'Адрес:',
-		5: 'г. Тольятти, ш. Южное, зд 163а, к 2/4, офис А316',
+	0: `Наше Портфолио`,
+	1: `Сайт АльВиРити Studio`,
+	2: `Представляем вашему вниманию один из сайтов IT компании «АльВиРити».<br><br>
+	Представленный сайт позволит вам ознакомится поближе со спецификой работы компании, 
+	и так же вы получите возможность познакомится со всеми членами дружной команды «АльВиРити».`,
+	3: `Сайт АльВиРити VR/AVR`,
+	4: `Представляем вашему вниманию второй сайт нашей IT компании.<br><br>
+	На сайте вы можете заказать комплект оказываемых услуг.`,
+	5: `Сайт руководителя компании «Алексея Воробьева»`,
+	6: `Данный сайт создавался лично для руководителя IT компании «АльВиРити», Алексея Воробьева.<br><br>
+	На сайте вы можете познакомиться с его автобиографией.`,
+	7: `Сайт компании «ТИС»`,
+	8: `Сайт для компании ООО «ТИС».<br><br>
+	Данная компания занимается производством качественных уплотнений и пластин для пластичных теплообменников.`,
+	9: `Avia-realtor, Montenegro`,
+	10: `Cайт разрабатывался компанией, занимающейся оформлением вида на жительство и покупкой недвижимости в Черногории и Турции.`,
+	11: `Сайт фитнес-центра «NEO»`,
+	12: `Сайт для фитнес центра «NEO» разработан по заказу технопарка в сфере высоких технологий "Жигулёвская долина".<br><br>
+	Мы также внедрили ПО для автоматизации администрационных процессов компании.`,
+	13: `Сайт по оказанию адвокатских услуг`,
+	14: `Сайт создавался опытными дизайнерам для многоуважаемого адвоката Русяевой Ольги Александровны.`,
+	15: `Сайт «Центр кадастровой оценки<br>Липецкой области»`,
+	16: `Сайт разрабатывался для «Центра кадастровой оценки Липецкой области».`,
 	},
 }
 
@@ -140,8 +135,10 @@ assets.english = {
 	22: `GET A DETAILED COMMERCIAL OFFER`,
 	23: `Calculate the price`,
 	24: `Contact us`,
-	25: `<x style='font-weight: 600; color: var(--accent)'>Contact phone:</x><br>
-	<x class='ref' onclick='href("tel:+79277888882")'>+7 (927) 788-88-82</x>`,
+	25: `<x style='font-weight: 600; color: var(--accent)'>Management contacts:</x><br>
+	<x class='ref' onclick='href("tel:+79277888882")'>+7 (927) 788-88-82</x><br>
+	<x class='ref' onclick='href("https://t.me/+79277888882")'>t.me/asvorobyov</x><br>
+	<x class='ref' onclick='href("https://wa.me/79277888882")'>wa.me/79277888882</x>`,
 	26: `<x style='font-weight: 600; color: var(--accent)'>E-mail:</x><br>
 	<x class='ref' onclick='href("mailto: AVorobyovS@ya.ru")'>AVorobyovS@ya.ru</x>`,
 	27: `<x style='font-weight: 600; color: var(--accent)'>Our socials:</x><br>
@@ -155,54 +152,47 @@ assets.english = {
 	31: ``,
 	},
 	p2t: {
-		0: `<x style='font: var(--fontTitle)'>What do we offer</x><br><br>
-		«AlViRity» has experience in the following fields:<br>VR development, software, websites, mobile applications, graphic design, marketing.`,
-		1: `VR/AR Apps`,
-		2: `Our experts will create for you the best project in the field of virtual and augmented reality.`,
-		3: `Mobile Apps`,
-		4: `This is a fast and convenient software designed to work on smartphones, tablets and other mobile devices.`,
-		5: `Software Development`,
-		6: `To write software, we rely on a development methodology.<br>Individual Software will increase profits up to 50%.`,
-		7: `Web Development`,
-		8: `High-quality Frontend and Backend development is the key to the success of your web project.
-		<br>Make an order for comprehensive website development services - take your business to a new level.`,
-		9: `Pop-Art Portraits`,
-		10: `We'll create your individual portrait in Pop-Art processing.`,
-		11: `More coming soon...`,
-		12: ``,
-		13: ``,
-		14: ``,
+	0: `<x style='font: var(--fontTitle)'>What do we offer</x><br><br>
+	«AlViRity» has experience in the following fields:<br>VR development, software, websites, mobile applications, graphic design, marketing.`,
+	1: `VR/AR Apps`,
+	2: `Our experts will create for you the best project in the field of virtual and augmented reality.`,
+	3: `Mobile Apps`,
+	4: `This is a fast and convenient software designed to work on smartphones, tablets and other mobile devices.`,
+	5: `Software Development`,
+	6: `To write software, we rely on a development methodology.<br>Individual Software will increase profits up to 50%.`,
+	7: `Web Development`,
+	8: `High-quality Frontend and Backend development is the key to the success of your web project.
+	<br>Make an order for comprehensive website development services - take your business to a new level.`,
+	9: `Pop-Art Portraits`,
+	10: `We'll create your individual portrait in Pop-Art processing.`,
+	11: `More coming soon...`,
+	12: ``,
+	13: ``,
+	14: ``,
 	},
 	p3t: {
-		0: `Our portfolio`,
-		1: `Alvirity Studio website`,
-		2: `We present to your attention one of the sites of the IT company «AlViRity».<br><br>
-		The presented website will allow you to get acquainted with the specifics of the company's work, 
-		and also you will get the opportunity to get acquainted with all the members of the friendly team «AlViRity».`,
-		3: `Alvirity VR/AVR website`,
-		4: `We present to your attention the second site of our IT company.<br><br>
-		On the website you can order a set of services provided.`,
-		5: `Website of the company founder, Alexey Vorobyov`,
-		6: `This website was created personally for the founder of the «AlViRity» company, Alexey Vorobyov.<br><br>
-		On the website you can get acquainted with his autobiography.`,
-		7: `«ТИС» Company website`,
-		8: `Website for «ТИС» LLL.<br><br>
-		This company is engaged in the production of high-quality seals and plates for plastic heat exchangers.`,
-		9: `Avia-realtor, Montenegro`,
-		10: `The site was developed by a company engaged in obtaining a residence permit and buying real estate in Montenegro and Turkey.`,
-		11: `«NEO» Fitness center website`,
-		12: `Website for the fitness center «NEO».<br>Developed by order of the technopark in the field of high technologies «Zhigulevskaya Dolina».
-		<br><br>We have also implemented software to automate the company's administrative processes.`,
-		13: `Lawyer services website`,
-		14: `The site was created by experienced designers for the esteemed lawyer Rusyaeva Olga Aleksandrovna.`,
-		15: `«Cadastral Valuation Center of the Lipetsk Region» Website`,
-		16: `This website was developed for «Cadastral Valuation Center of the Lipetsk Region».`,
-	},
-	fooP: {
-		1: 'Resident of the Zhigulevskaya Dolina',
-		2: 'CONTACTS',
-		3: 'Phone number:',
-		4: 'Address:',
-		5: 'Togliatti, st. Yuzhnoe Shosse, 163A  b2/4, A316',
+	0: `Our portfolio`,
+	1: `Alvirity Studio website`,
+	2: `We present to your attention one of the sites of the IT company «AlViRity».<br><br>
+	The presented website will allow you to get acquainted with the specifics of the company's work, 
+	and also you will get the opportunity to get acquainted with all the members of the friendly team «AlViRity».`,
+	3: `Alvirity VR/AVR website`,
+	4: `We present to your attention the second site of our IT company.<br><br>
+	On the website you can order a set of services provided.`,
+	5: `Website of the company founder, Alexey Vorobyov`,
+	6: `This website was created personally for the founder of the «AlViRity» company, Alexey Vorobyov.<br><br>
+	On the website you can get acquainted with his autobiography.`,
+	7: `«ТИС» Company website`,
+	8: `Website for «ТИС» LLL.<br><br>
+	This company is engaged in the production of high-quality seals and plates for plastic heat exchangers.`,
+	9: `Avia-realtor, Montenegro`,
+	10: `The site was developed by a company engaged in obtaining a residence permit and buying real estate in Montenegro and Turkey.`,
+	11: `«NEO» Fitness center website`,
+	12: `Website for the fitness center «NEO».<br>Developed by order of the technopark in the field of high technologies «Zhigulevskaya Dolina».
+	<br><br>We have also implemented software to automate the company's administrative processes.`,
+	13: `Lawyer services website`,
+	14: `The site was created by experienced designers for the esteemed lawyer Rusyaeva Olga Aleksandrovna.`,
+	15: `«Cadastral Valuation Center of the Lipetsk Region» Website`,
+	16: `This website was developed for «Cadastral Valuation Center of the Lipetsk Region».`,
 	},
 }
