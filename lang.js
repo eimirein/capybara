ef3c4b84036f90e0d4926177530726cc96cac26a
霊音・永旻 <109428665+eimirein@users.noreply.github.com>
2022-12-08 19:53:36 +0400

    Add files via upload

---
+++ lang.js
@@ -0,0 +1,208 @@
+lang_list = ['english', 'russian']
+
+assets.russian = {
+	title: ['АльВиРити'],
+	t: {
+	0: `Ваш город: Тольятти`,
+	1: `МЕНЮ`,
+	2: `Коммерческое<br>предложение`,
+	3: `Команда`,
+	4: `Услуги`,
+	5: `Проекты`,
+	6: `Контакты`,
+	7: `Позвонить`,
+	8: `ИНТЕРНЕТ ПРОЕКТЫ`,
+	9: `От идеи до реализации<br><br>Разрабатываем и реализовываем для Вас самые смелые решения программного обеспечения IT индустрии`,
+	10: `Открой для себя наши коллекции`,
+	11: `Самые популярные услуги`,
+	12: `Создание сайта`,
+	13: `Приложения VR/AR`,
+	14: `Создание мобильных приложений`,
+	15: `Интернет маркетинг`,
+	16: `Портреты Pop-Art`,
+	17: `Подробнее`,
+	18: `Мнение наших экспертов`,
+	19: `<x style='font: var(--fontTitle)'>Иван Бутрим</x><br>
+	<x style='font: var(--fontSub)'>Главный Дизайнер</x><br><br>
+	Добрый день, моё имя Иван, и я являюсь руководитель отдела графического дизайна.<br><br>Мы 
+	занимаемся разработкой визуального оформления и структурирования сайта по всем пожеланиям 
+	заказчика. При создании дизайна мы вкладываем все свои навыки, и делаем свою работу безупречно.`,
+	20: `<x style='font: var(--fontTitle)'>Алексей Воробьев</x><br>
+	<x style='font: var(--fontSub)'>Руководитель компании</x><br><br>
+	Я и моя команда занимаемся разработкой программного обеспечения от обычных сайтов 
+	до полноценных автоматизированных информационных систем.<br>Также создаем приложения для VR/AR.<br><br>Рады партнерам и инвесторам.`,
+	21: `<x style='font: var(--fontTitle)'>Тимур Акчурин</x><br>
+	<x style='font: var(--fontSub)'>Дизайнер</x><br><br>Всем привет, меня зовут Тимур и я в пиджаке. `,
+	22: `ПОЛУЧИТЕ ПОДРОБНОЕ КОММЕРЧЕСКОЕ ПРЕДЛОЖЕНИЕ`,
+	23: `Рассчитать стоимость`,
+	24: `Свяжитесь с нами`,
+	25: `<x style='font-weight: 600; color: var(--accent)'>Номер для связи:</x><br>
+	<x class='ref' onclick='href("tel:+79277888882")'>+7 (927) 788-88-82</x>`,
+	26: `<x style='font-weight: 600; color: var(--accent)'>E-mail:</x><br>
+	<x class='ref' onclick='href("mailto: AVorobyovS@ya.ru")'>AVorobyovS@ya.ru</x>`,
+	27: `<x style='font-weight: 600; color: var(--accent)'>Мы в соцсетях:</x><br>
+	<x class='ref' onclick='href("https://vk.com/alvirity")'>https://vk.com/alvirity</x><br>
+	<x class='ref' onclick='href("https://vk.com/alvirity.studio")'>https://vk.com/alvirity.studio</x><br>
+	<x class='ref' onclick='href("https://t.me/generatorofthefuture")'>https://t.me/generatorofthefuture</x>`,
+	28: `<x style='font-weight: 600; color: var(--accent)'>Главный офис:</x><br>
+	<x class='ref' onclick='href("https://goo.gl/maps/qCFrD4rxLQt4912z9")'>г. Тольятти, ул. Южное шоссе, д. 163А к2/4</x>`,
+	29: `Я согласен(а) на обработку персональных данных`,
+	30: ``,
+	31: ``,
+	},
+	p2t: {
+		0: `<x style='font: var(--fontTitle)'>Что мы можем предложить</x><br><br>
+		«АльВиРити» имеет опыт в сферах:<br>VR разработки, ПО, веб-сайтов, мобильных приложений, графического дизайна, маркетинге.`,
+		1: `VR/AR разработка`,
+		2: `Наши специалисты создадут для вас лучший проект в области виртуальной и дополненной реальности`,
+		3: `Мобильные приложения`,
+		4: `Это быстрое и удобное ПО, предназначенное для работы на смартфонах, планшетах и других мобильных устройствах`,
+		5: `Разработка ПО`,
+		6: `Для написания программного обеспечения мы основываемся на методологии разработки.
+		<br>Индивидуальное Програмное обеспечение увеличит прибыль до 50%`,
+		7: `Веб-разработка`,
+		8: `Качественная Frontend и Backend разработка - залог успешной работы вашего веб-проекта.
+		<br>Сделайте заказ комплексных услуг по созданию сайта - переведите ваш бизнес на новый уровень`,
+		9: `Портреты Pop-Art`,
+		10: `Создадим ваш индивидуальный портрет в Pop-Art обработке`,
+		11: `Еще больше ожидается...`,
+		12: ``,
+		13: ``,
+		14: ``,
+	},
+	p3t: {
+		0: `Наше Портфолио`,
+		1: `Сайт АльВиРити Studio`,
+		2: `Представляем вашему вниманию один из сайтов IT компании «АльВиРити».<br><br>
+		Представленный сайт позволит вам ознакомится поближе со спецификой работы компании, 
+		и так же вы получите возможность познакомится со всеми членами дружной команды «АльВиРити».`,
+		3: `Сайт АльВиРити VR/AVR`,
+		4: `Представляем вашему вниманию второй сайт нашей IT компании.<br><br>
+		На сайте вы можете заказать комплект оказываемых услуг.`,
+		5: `Сайт руководителя компании «Алексея Воробьева»`,
+		6: `Данный сайт создавался лично для руководителя IT компании «АльВиРити», Алексея Воробьева.<br><br>
+		На сайте вы можете познакомиться с его автобиографией.`,
+		7: `Сайт компании «ТИС»`,
+		8: `Сайт для компании ООО «ТИС».<br><br>
+		Данная компания занимается производством качественных уплотнений и пластин для пластичных теплообменников.`,
+		9: `Avia-realtor, Montenegro`,
+		10: `Cайт разрабатывался компанией, занимающейся оформлением вида на жительство и покупкой недвижимости в Черногории и Турции.`,
+		11: `Сайт фитнес-центра «NEO»`,
+		12: `Сайт для фитнес центра «NEO» разрабатывался по заказу Технопарка-Жигулёвская Долина.`,
+		13: `Сайт по оказанию адвокатских услуг`,
+		14: `Сайт создавался опытными дизайнерам для многоуважаемого Адвоката «Ольги Русяевой».`,
+		15: `Сайт «Центр кадастровой оценки Липецкой области»`,
+		16: `Сайт разрабатывался для «Центра кадастровой оценки Липецкой области».`,
+		C: `© АльВиРити - 2022  |  Все права защищены`
+	},
+	fooP: {
+		1: 'Резидент Технопарка Жигулёвская долина',
+		2: 'КОНТАКТЫ',
+		3: 'Телефон:',
+		4: 'Адрес:',
+		5: 'г. Тольятти, ш. Южное, зд 163а, к 2/4, офис А316',
+	},
+}
+
+assets.english = {
+	title: ['Alvirity'],
+	t: {
+	0: `Your city: Togliatti`,
+	1: `MENU`,
+	2: `Commercial Offer`,
+	3: `Team`,
+	4: `Services`,
+	5: `Projects`,
+	6: `Contacts`,
+	7: `Call me`,
+	8: `INTERNET PROJECTS`,
+	9: `From an idea to realization<br><br>We develop and actualize for you the bravest software solutions of the IT industry`,
+	10: `Discover our collections`,
+	11: `Most popular services`,
+	12: `Website creation`,
+	13: `VR/AR applications`,
+	14: `Mobile app creation`,
+	15: `Internet marketing`,
+	16: `Pop-Art portraits`,
+	17: `Read more`,
+	18: `Opinions of our experts`,
+	19: `<x style='font: var(--fontTitle)'>Ivan Butrim</x><br>
+	<x style='font: var(--fontSub)'>Lead designer</x><br><br>
+	Good day, my name is Ivan, and I am the head of the graphic design department.<br><br>We are 
+	engaged in the development of visual design and website structuring for all customer requirements. 
+	When creating a design, we put all of our skills into it, and we do our job flawlessly.`,
+	20: `<x style='font: var(--fontTitle)'>Alexey Vorobyov</x><br>
+	<x style='font: var(--fontSub)'>Company founder</x><br><br>
+	Me and my team are engaged in software development from ordinary websites to full-fledged 
+	automated information systems.<br>We also create VR/AR applications.<br><br>We welcome partners and investors.`,
+	21: `<x style='font: var(--fontTitle)'>Timur Akchurin</x><br>
+	<x style='font: var(--fontSub)'>Designer</x><br><br>Howdy y'all, my name's Timur and I wear a suit.`,
+	22: `GET A DETAILED COMMERCIAL OFFER`,
+	23: `Calculate the price`,
+	24: `Contact us`,
+	25: `<x style='font-weight: 600; color: var(--accent)'>Contact phone:</x><br>
+	<x class='ref' onclick='href("tel:+79277888882")'>+7 (927) 788-88-82</x>`,
+	26: `<x style='font-weight: 600; color: var(--accent)'>E-mail:</x><br>
+	<x class='ref' onclick='href("mailto: AVorobyovS@ya.ru")'>AVorobyovS@ya.ru</x>`,
+	27: `<x style='font-weight: 600; color: var(--accent)'>Our socials:</x><br>
+	<x class='ref' onclick='href("https://vk.com/alvirity")'>https://vk.com/alvirity</x><br>
+	<x class='ref' onclick='href("https://vk.com/alvirity.studio")'>https://vk.com/alvirity.studio</x><br>
+	<x class='ref' onclick='href("https://t.me/generatorofthefuture")'>https://t.me/generatorofthefuture</x>`,
+	28: `<x style='font-weight: 600; color: var(--accent)'>Head quarters:</x><br>
+	<x class='ref' onclick='href("https://goo.gl/maps/qCFrD4rxLQt4912z9")'>Togliatti, st. Yuzhnoe Shosse, 163A b2/4, A316</x>`,
+	29: `I agree to the processing of personal data`,
+	30: ``,
+	31: ``,
+	},
+	p2t: {
+		0: `<x style='font: var(--fontTitle)'>What do we offer</x><br><br>
+		«AlViRity» has experience in the following fields:<br>VR development, software, websites, mobile applications, graphic design, marketing.`,
+		1: `VR/AR Apps`,
+		2: `Our experts will create for you the best project in the field of virtual and augmented reality.`,
+		3: `Mobile Apps`,
+		4: `This is a fast and convenient software designed to work on smartphones, tablets and other mobile devices.`,
+		5: `Software Development`,
+		6: `To write software, we rely on a development methodology.<br>Individual Software will increase profits up to 50%.`,
+		7: `Web Development`,
+		8: `High-quality Frontend and Backend development is the key to the success of your web project.
+		<br>Make an order for comprehensive website development services - take your business to a new level.`,
+		9: `Pop-Art Portraits`,
+		10: `We'll create your individual portrait in Pop-Art processing.`,
+		11: `More coming soon...`,
+		12: ``,
+		13: ``,
+		14: ``,
+	},
+	p3t: {
+		0: `Our portfolio`,
+		1: `Alvirity Studio website`,
+		2: `We present to your attention one of the sites of the IT company «AlViRity».<br><br>
+		The presented website will allow you to get acquainted with the specifics of the company's work, 
+		and also you will get the opportunity to get acquainted with all the members of the friendly team «AlViRity».`,
+		3: `Alvirity VR/AVR website`,
+		4: `We present to your attention the second site of our IT company.<br><br>
+		On the website you can order a set of services provided.`,
+		5: `Website of the company founder, Alexey Vorobyov`,
+		6: `This website was created personally for the founder of the «AlViRity» company, Alexey Vorobyov.<br><br>
+		On the website you can get acquainted with his autobiography.`,
+		7: `«ТИС» Company website`,
+		8: `Website for «ТИС» LLL.<br><br>
+		This company is engaged in the production of high-quality seals and plates for plastic heat exchangers.`,
+		9: `Avia-realtor, Montenegro`,
+		10: `The site was developed by a company engaged in obtaining a residence permit and buying real estate in Montenegro and Turkey.`,
+		11: `«NEO» Fitness center website`,
+		12: `Website for the fitness center «NEO».<br>Was developed by order of the «Zhigulevskaya Dolina Technopark».`,
+		13: `Lawyer services website`,
+		14: `The site was created by experienced designers for the esteemed lawyer Olga Rusyaeva.`,
+		15: `«Cadastral Valuation Center of the Lipetsk Region» Website`,
+		16: `This website was developed for «Cadastral Valuation Center of the Lipetsk Region».`,
+		C: `© Alvirity - 2022  |  All rights reserved`
+	},
+	fooP: {
+		1: 'Resident of the Zhigulevskaya Dolina',
+		2: 'CONTACTS',
+		3: 'Phone number:',
+		4: 'Address:',
+		5: 'Togliatti, st. Yuzhnoe Shosse, 163A  b2/4, A316',
+	},
+}
---
+++ main.js
@@ -0,0 +1,313 @@
+vh = 1 * window.innerHeight/100;  vw = 1 * window.innerWidth/100
+temp = []; assets = []; i_lang = -1
+function emi(id) { if (document.getElementById(id)) { return document.getElementById(id) } }
+function percent(this_num, of_the) { return Math.round( (100 * this_num) / of_the ) }
+function print(...msg) {
+	var em = emi("log")
+	msg.forEach(function(message) {
+		if (em) {
+			em.innerHTML = em.innerHTML + '['+time()+'] '+message
+			em.appendChild(document.createElement('br'))
+		}
+		console.log('['+time()+'] '+message)
+	})
+}
+function log(msg, lvl) {
+	var em = emi("log")
+	var levels = ['userdata', 'info', 'warning', 'error', 'crash']
+	if (em) {
+		em.innerHTML = em.innerHTML + '['+time()+'] <@'+levels[lvl || 0]+'> '+msg
+		em.appendChild(document.createElement('br'))
+	}
+	console.log('['+time()+'] <@'+levels[lvl || 0]+'> '+msg)
+}
+function time() {
+	var get_time = new Date()
+	var hour = get_time.getHours()
+	var min = get_time.getMinutes()
+	var sec = get_time.getSeconds()
+	if (hour.toString().length==1) {hour = '0'+hour}
+	if (min.toString().length==1) {min = '0'+min}
+	if (sec.toString().length==1) {sec = '0'+sec}
+	return hour+":"+min+':'+sec
+}
+function fullscreen() {
+	var em = document.body
+	if ( !em.fs || em.fs === 'false' ) {
+		if (em.requestFullscreen) { em.requestFullscreen() }
+		else if (em.webkitRequestFullscreen) { em.webkitRequestFullscreen() }
+		else if (em.msRequestFullscreen) { em.msRequestFullscreen() }
+		em.fs = 'true'
+	}
+	else if ( em.fs === 'true' ) {
+		if (document.exitFullscreen) { document.exitFullscreen() }
+		else if (document.webkitExitFullscreen) { document.webkitExitFullscreen() }
+		else if (document.msExitFullscreen) { document.msExitFullscreen() }
+		em.fs = 'false'
+	}
+}
+function href(https, _b) {
+	if (_b) { window.open(https, '_blank') }
+	else { window.open(https, '_self') }
+}
+// ex: assets.lang = { title: { 1: 'a', 2: 'b' }, pgph: { 1: 'hi', 2: 'bye' } }
+function langs(name) {
+	if (!name) {
+		if (i_lang < lang_list.length-1) { i_lang++ } else { i_lang = 0 }
+		lang = lang_list[i_lang]
+	} else { lang = lang_list[name] }
+	for (let sector in assets[lang]) {
+		for (let i in assets[lang][sector]) {
+			if ( emi(sector+i) ) { emi(sector+i).innerHTML = assets[lang][sector][i] }
+		}
+	}
+	if (emi('lang')) { emi('lang').innerHTML = lang.toUpperCase() }
+	emi('html').lang = lang
+}
+// Set/change CSS variable
+function pattern(CSSVar, val) { document.documentElement.style.setProperty('--'+CSSVar, val) }
+// Scroll to an element
+function scroll2(id) { emi(id).scrollIntoView() }
+// Set functions for device type
+function vport(func_desktop, func_mobile) {
+	let h = window.innerHeight
+	let w = window.innerWidth
+	if ( percent(h, w) > 100 ) { func_mobile() }
+	else { func_desktop() }
+}
+
+// Require a script into HTML
+function require(source) {
+	var script = document.createElement('script')
+	script.src = source
+	document.documentElement.appendChild(script)
+}
+// Load JS script / CSS sheet from a string
+function loadstring(str) {
+	var script = document.createElement('script')
+	script.innerHTML = str
+	document.documentElement.appendChild(script)
+}
+function loadsheet(str) {
+	var style = document.createElement('style')
+	style.innerHTML = str
+	document.documentElement.appendChild(style)
+}
+
+// Copy text from an element to clipboard
+function clip(id) {
+	if ( emi(id) ) {
+		navigator.clipboard.writeText(emi(id).textContent)
+	} else { log('clip :: Element with id ['+id+'] does not exist', 1) }
+}
+// Insert text into an element
+function type(id, txt) {
+	if ( emi(id) ) {
+		emi(id).appendChild(document.createTextNode(txt))
+		emi(id).appendChild(document.createElement('br'))
+	} else { log('type :: Element with id ['+id+'] does not exist', 1) }
+}
+// Erase text data of an element
+function erase(id) {
+	if ( emi(id) ) {
+		emi(id).textContent = ''
+	} else { log('erase :: Element with id ['+id+'] does not exist', 1) }
+}
+// Erase all internal data of an element
+function wipe(id) {
+	if ( emi(id) ) {
+		emi(id).innerHTML = ''
+	} else { log('wipe :: Element with id ['+id+'] does not exist', 1) }
+}
+// Remove one or multiple elements
+function rm(...ids) {
+	ids.forEach(function(id) {
+		if ( emi(id) ) {
+			emi(id).remove()
+		} else { log('rm :: Element with id ['+id+'] does not exist', 1) }
+	})
+}
+// Make one or multiple elements/write data within a specific root body
+function mk(root_id, html_or_array) {
+	root = emi(root_id)
+	if ( root ) {
+		if ( typeof html_or_array === 'object' ) {
+			for (var index in html_or_array) {
+				root.innerHTML = root.innerHTML + html_or_array[index]
+			}
+		} else { root.innerHTML = root.innerHTML + html_or_array }
+	} else { log('mk :: Root element with id ['+root_id+'] does not exist', 1) }
+}
+
+// Animate an element, optionally add a second animation and set an interval
+function a8(id, animation, a8opt, int_opt) {
+	var a8em = emi(id)
+	if ( a8em ) {
+		if ( !a8opt && !int_opt ) { a8em.style.animation = animation }
+		else {
+			setInterval( function() { a8em.style.animation = animation }, int_opt )
+			setInterval( function() { a8em.style.animation = a8opt }, int_opt*2 )
+		}
+	} else { log('a8 :: Element with id ['+id+'] does not exist', 1) }
+}
+// Add a switch-state expression to an element and assign 2 animations to it
+function a8ss(id, a8A, a8B) {
+	var a8em = emi(id)
+	if ( a8em ) {
+		if ( !a8em.value || a8em.value === 'false' ) {
+			a8em.style.animation = a8A
+			a8em.value = 'true'
+		}
+		else if ( a8em.value === 'true' ) {
+			a8em.style.animation = a8B
+			a8em.value = 'false'
+		}
+	} else { log('a8 :: Element with id ['+id+'] does not exist', 1) }
+}
+// Animate multiple objects on interval
+// <str: name> Name for a loop; <int: interval> Execution interval (ms);
+// <array: table> Array with element IDs; <str: a8x2> CSS Animations
+function a8x(name, interval, table, a8show, a8hide) {
+	if (!temp[name+'_i']) { temp[name+'_i'] = -1 } // Set [i] for loop
+	if (!temp[name+'Switch']) { temp[name+'Switch'] = true } // Set a switch
+	setInterval( function() {
+		if ( temp[name+'Switch'] ) {
+			if ( temp[name+'_i'] < table.length-1 ) {
+				temp[name+'_i']++
+				a8(table[ temp[name+'_i'] ], a8hide)
+			} else {
+				temp[name+'_i'] = table.length
+				temp[name+'Switch'] = false
+			} // {a, b, c} ex :: (0.hide[init]) -> 1.hide -> 2.hide -> false
+		}
+		if ( !temp[name+'Switch'] ) {
+			if ( temp[name+'_i'] > 0 ) {
+				temp[name+'_i']--
+				a8(table[ temp[name+'_i'] ], a8show)
+			} else {
+				temp[name+'_i'] = 0
+				temp[name+'Switch'] = true
+				a8(table[ temp[name+'_i'] ], a8hide)
+			} // {a, b, c} ex :: 2.show -> 1.show -> 0.show -> true -> 0.hide
+		}
+	}, interval) // P.S: Not gonna debug it, it's already huge and complicated
+}
+
+// Add a switch-state expression to an element and assign 2 functions to it
+function trig(id, funcON, funcOFF) {
+	em = emi(id)
+	if (em) {
+		if ( !em.state || em.state === 'OFF' ) {
+			funcON()
+			em.state = 'ON'
+		}
+		else if ( em.state === 'ON' ) {
+			funcOFF()
+			em.state = 'OFF'
+		}
+	}
+	else { log('trig :: Element with id ['+id+'] does not exist', 1) }
+}
+// Slider widget function
+//1. Create a bunch of elements | Ex: 3 divs with [id] 'cat1', 'cat2' and 'cat3'
+//2. Initiate them in the body.onload() | Ex: slide('cat', 1, 3) or slide('cat', 'R', 3)
+//3. Add selection function | Ex: <div class='EX' id='cat2' onclick="slide('cat', 2)"></div>
+//4. Bind scroll function | Ex: <div id='nextCat' onclick="slide('cat', 'R')"></div>
+//5. Set styles for given elements | Ex: .EX.slided {opacity: 0.2} ; .EX.slide {opacity: 1}
+function slide(id, dir, _init) {
+	// Create [slides] and slides[id] arrays if they doesn't exist
+	if (typeof slides == 'undefined') { slides = {} }
+	if (!slides[id] && _init) { slides[id]=[]; for (let i=0; i<(_init+1); i++) { slides[id].push(i) } }
+	if (dir=='R') { // Change class for this [id] and switch to next
+		if (emi(id+slides[id][0])) {
+			emi(id+slides[id][0]).classList.remove("slide", "slideR", "slideL")
+			emi(id+slides[id][0]).style.animation = null
+			emi(id+slides[id][0]).classList.add("slided")
+		}
+		slides[id][0] = slides[id][0]+1
+		if (emi(id+slides[id][0])) { // Change class for the new [id]
+			emi(id+slides[id][0]).classList.remove("slided")
+			emi(id+slides[id][0]).classList.add("slide", "slideR")
+		} else { // If next [id] doesn't exist, set index to 1 and change class
+			slides[id][0] = 1
+			emi(id+slides[id][0]).classList.remove("slided")
+			emi(id+slides[id][0]).classList.add("slide", "slideR")
+		}
+	}
+	if (dir=='L') { // Change class for this [id] and switch to previous
+		if (emi(id+slides[id][0])) {
+			emi(id+slides[id][0]).classList.remove("slide", "slideL", "slideR")
+			emi(id+slides[id][0]).style.animation = null
+			emi(id+slides[id][0]).classList.add("slided")
+		}
+		slides[id][0] = slides[id][0]-1
+		if (emi(id+slides[id][0])) { // Change class for the new [id]
+			emi(id+slides[id][0]).classList.remove("slided")
+			emi(id+slides[id][0]).classList.add("slide", "slideL")
+		} else { // If previous [id] doesn't exist, set index to [max] and change class
+			slides[id][0] = (slides[id].length-1)
+			emi(id+slides[id][0]).classList.remove("slided")
+			emi(id+slides[id][0]).classList.add("slide", "slideL")
+		}
+	}
+	// Check if [id] with [dir] index exists and change classes for this and selected [id]
+	if (typeof dir=='number' && emi(id+dir)) {
+		if (emi(id+slides[id][0])) {
+			emi(id+slides[id][0]).classList.remove("slide", "slideR", "slideL")
+			emi(id+slides[id][0]).style.animation = null
+			emi(id+slides[id][0]).classList.add("slided")
+		}
+		slides[id][0] = dir
+		emi(id+slides[id][0]).classList.remove("slided")
+		emi(id+slides[id][0]).classList.add("slide")
+	} //2022.11.05 :: Assigns 3 classes, clears animation states
+}
+
+// Scroll to top/bottom on call
+function scroll_top(id) {
+	if (id) { emi(id).scrollTop = 0 }
+	else { window.scroll(0,0) }
+}
+function scroll_bottom(id) {
+	if (id) { emi(id).scrollTop = emi(id).scrollHeight }
+	else { window.scrollTo(0, document.body.scrollHeight) }
+}
+// Trigger a function/animation when an element is visible at a certain depth in px
+function render() {
+	var scr = document.querySelectorAll(".scr")
+	for (var i = 0; i < scr.length; i++) {
+		var windowHeight = window.innerHeight
+		var elementTop = scr[i].getBoundingClientRect().top
+		var elementVisible = 150
+		if (elementTop < windowHeight - elementVisible) {
+			scr[i].classList.add("rend")
+		} else {
+			scr[i].classList.remove("rend")
+		}
+	}
+}
+
+function init() {
+	vport(
+		function(){ //desktop
+			pattern('fontMain', "400 2vmin 'Montserrat'")
+			pattern('fontTitle', "600 3.4vmin 'Montserrat'")
+			pattern('fontSub', "300 2.4vmin 'Montserrat'")
+			pattern('fontBtn', "500 2.6vmin 'Montserrat'")
+			pattern('fontHead', "400 1.7vmin 'Montserrat'")
+			pattern('logoW', '18vw'); pattern('topM', '10vw')
+		},
+		function(){ // mobile
+			pattern('fontMain', "400 2vmin 'Montserrat'")
+			pattern('fontTitle', "500 3.4vmin 'Montserrat'")
+			pattern('fontSub', "300 2.8vmin 'Montserrat'")
+			pattern('fontBtn', "500 2.6vmin 'Montserrat'")
+			pattern('fontHead', "400 1.7vmin 'Montserrat'")
+			pattern('logoW', '26vw'); pattern('topM', '4vw')
+		}
+	)
+}
+
+// Window events
+window.addEventListener('resize', function(){ init() }, false)
+window.addEventListener("scroll", render); render()
